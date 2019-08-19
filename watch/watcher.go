245e492491a58c153d02979c555b0fc88ff1f045
Henry Wallace <h@henrywallace.io>
2019-08-18 23:59:42 -0400

    go/netwatch: allow pub/sub style functions as input to Watcher
    Where currently there is only a Logger subscriber.

--- watch/watcher.go
+++ watch/watcher.go
@@ -19,12 +19,19 @@ var (
 type Watcher struct {
 	log    *logrus.Logger
 	events chan Event
+	subs   []Subscriber
 }
 
-func NewWatcher(log *logrus.Logger) *Watcher {
+type Subscriber func(e Event) error
+
+func NewWatcher(log *logrus.Logger, subs ...Subscriber) *Watcher {
+	if len(subs) == 0 {
+		subs = []Subscriber{Logger(log)}
+	}
 	return &Watcher{
 		log:    log,
 		events: make(chan Event, 32),
+		subs:   subs,
 	}
 }
 
@@ -40,33 +47,44 @@ func (w *Watcher) Watch(ctx context.Context) error {
 	src := gopacket.NewPacketSource(h, h.LinkType())
 	hosts := make(map[MAC]*Host)
 	go w.ScanPackets(hosts, src.Packets())
-	return w.Respond()
+	return w.Publish()
 }
 
-func (w *Watcher) Respond() error {
+func (w *Watcher) Publish() error {
 	for e := range w.events {
-		switch e.Type {
-		case HostNew:
-			e := e.Body.(EventHostNew)
-			w.log.Infof("new %s", e.Host)
-		case HostDrop:
-			e := e.Body.(EventHostDrop)
-			w.log.Infof("drop %s (up %s)", e.Host, e.Up)
-		case HostReturn:
-			e := e.Body.(EventHostReturn)
-			w.log.Infof("return %s (down %s)", e.Host, e.Down)
-		case PortNew:
-			e := e.Body.(EventPortNew)
-			w.log.Infof("new %s on %s", e.Port, e.Host)
-		case PortDrop:
-			e := e.Body.(EventPortDrop)
-			w.log.Infof("drop %s (up %s) on %s", e.Port, e.Up, e.Host)
-		case PortReturn:
-			e := e.Body.(EventPortReturn)
-			w.log.Infof("return %s (down %s) on %s", e.Port, e.Down, e.Host)
-		default:
-			panic(fmt.Sprintf("unhandled event type: %#v", e))
+		for _, sub := range w.subs {
+			if err := sub(e); err != nil {
+				w.log.WithError(err).Errorf("failed to respond to event")
+			}
 		}
 	}
 	return nil
+}
+
+func Logger(log *logrus.Logger) Subscriber {
+	return func(e Event) error {
+		switch e.Type {
+		case HostNew:
+			e := e.Body.(EventHostNew)
+			log.Infof("new %s", e.Host)
+		case HostDrop:
+			e := e.Body.(EventHostDrop)
+			log.Infof("drop %s (up %s)", e.Host, e.Up)
+		case HostReturn:
+			e := e.Body.(EventHostReturn)
+			log.Infof("return %s (down %s)", e.Host, e.Down)
+		case PortNew:
+			e := e.Body.(EventPortNew)
+			log.Infof("new %s on %s", e.Port, e.Host)
+		case PortDrop:
+			e := e.Body.(EventPortDrop)
+			log.Infof("drop %s (up %s) on %s", e.Port, e.Up, e.Host)
+		case PortReturn:
+			e := e.Body.(EventPortReturn)
+			log.Infof("return %s (down %s) on %s", e.Port, e.Down, e.Host)
+		default:
+			panic(fmt.Sprintf("unhandled event type: %#v", e))
+		}
+		return nil
+	}
 }
